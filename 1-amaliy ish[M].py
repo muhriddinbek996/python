cb2b044d45ad40e75f0d22ec7850465828819ca5
muhriddinbek996 <114127858+muhriddinbek996@users.noreply.github.com>
2022-09-26 10:19:55 +0500

    Add files via upload

---
+++ 1-amaliy ish[M].py
@@ -0,0 +1,8 @@
+x=int(input("mahsulot kilogramini kiriting x="))
+narx=float(input("mahsuloat summasini kiriting narx ="))
+x1=narx/x
+print(f"Mahsulotning 1kg uchun narx = {x1}")
+
+ixtiyoriy_kg=int(input("Necha kilogram narxini hisoblamoqchisiz -> "))
+natija=ixtiyoriy_kg*x1
+print(f"{ixtiyoriy_kg} kg uchun narx {natija}")
